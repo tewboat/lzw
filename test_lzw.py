ade85beebd451ee22e26d8862095d14d81d8349f
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2021-12-07 20:42:29 +0500

    add tests

--- test_lzw.py
+++ test_lzw.py
@@ -4,44 +4,41 @@ from lzw import Lzw
 
 class LzwTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.lzw = Lzw()
+
     def test_compression_1(self):
-        lzw = Lzw()
         data = b'\x00\x00\x00\x00\x00'
-        encoded_data, _ = lzw.encode(data)
+        encoded_data, _ = self.lzw.encode(data)
         self.assertEqual(b'\x00\x00\x01\x00\x01\x00', encoded_data)
 
     def test_compression_2(self):
-        lzw = Lzw()
         data = b'\x00\x01\x02\x03\x04'
-        encoded_data, _ = lzw.encode(data)
+        encoded_data, _ = self.lzw.encode(data)
         self.assertEqual(b'\x00\x00\x00\x01\x00\x02\x00\x03\x00\x04', encoded_data)
 
     def test_compression_3(self):
-        lzw = Lzw()
         data = b'\x00\x01\x00\x01\x02\x00\x01\x02'
-        encoded_data, _ = lzw.encode(data)
+        encoded_data, _ = self.lzw.encode(data)
         self.assertEqual(b'\x00\x00\x00\x01\x01\x00\x00\x02\x01\x02', encoded_data)
 
     def test_decompression(self):
-        lzw = Lzw()
         data = 'Hello world!'.encode()
-        encoded_data, _ = lzw.encode(data)
-        decoded_data = lzw.decode(encoded_data)
+        encoded_data, _ = self.lzw.encode(data)
+        decoded_data = self.lzw.decode(encoded_data)
         self.assertEqual(data, decoded_data)
 
     def test_decompression_russian(self):
-        lzw = Lzw()
         data = 'Привет мир! ЁёЁёЁ'.encode()
-        encoded_data, _ = lzw.encode(data)
-        decoded_data = lzw.decode(encoded_data)
+        encoded_data, _ = self.lzw.encode(data)
+        decoded_data = self.lzw.decode(encoded_data)
         self.assertEqual(data, decoded_data)
 
     def test_compressing_and_decompressing_big_data(self):
-        lzw = Lzw()
         with open('test_data/data.txt', 'rb') as test_data:
             data = test_data.read()
-            encoded_data, _ = lzw.encode(data)
-            decoded_data = lzw.decode(encoded_data)
+            encoded_data, _ = self.lzw.encode(data)
+            decoded_data = self.lzw.decode(encoded_data)
             self.assertEqual(data, decoded_data)
 
 
