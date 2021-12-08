a7d8613c255ee3df68609c299f83c7f159576872
Ilya <zdravulin@gmail.com>
2021-12-08 23:42:08 +0500

    add tests

--- test_archive.py
+++ test_archive.py
@@ -2,16 +2,21 @@ import os
 import shutil
 import unittest
 
+import exif
+
 from archiver import Archiver
 
 """
-    Format description:
+Format description:
 
-                Offset      Size
-    Name        0           128
-    Type        128         1       (0 for files, 1 for folders)
-    Size        129         4       (only for files)
-    Checksum    133         16      (only for files)
+                Size
+Name            128
+Type            1       (0 for files, 1 for folders)
+Checksum        16      (only for files)
+MetaDataSize    2       (only for files)
+MetaData        ...     (only for files)
+DataSize        4       (only for files)
+Data            ...     (only for files)
 """
 
 
@@ -25,13 +30,14 @@ class ArchiveTest(unittest.TestCase):
         shutil.rmtree('result', ignore_errors=True)
 
     def test_file_archivation(self):
-        expected = b"helloworld.py\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+        expected = b"helloworld.py\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+                   b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+                   b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
-                   b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
-                   b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
-                   b"\x00\x00\x00\x00\x00\x14g\x7f\xbd\xfa\xb6ze\xefu&\xc0)\xc5V\xb7\xe2print('hello world')"
+                   b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00g\x7f\xbd\xfa\xb6ze\xefu&\xc0)" \
+                   b"\xc5V\xb7\xe2\x00\x00\x00\x00\x00\x14print('hello world')"
         zipped = self.archiver.zip([os.path.join('test_data', 'helloworld.py')])[0]
         self.assertEqual(zipped, expected)
         self.archiver.unzip(zipped, 'result')
@@ -65,3 +71,12 @@ class ArchiveTest(unittest.TestCase):
             e = next(expected)
             self.assertEqual(i[1], e[1])
             self.assertEqual(i[2], e[2])
+
+    def test_metadata_archivation(self):
+        zipped = self.archiver.zip([os.path.join('test_data', 'EC7A7633.jpg')])[0]
+        self.archiver.unzip(zipped, 'result')
+        with open(os.path.join('test_data', 'EC7A7633.jpg'), 'rb') as file1:
+            with open(os.path.join('result', 'EC7A7633.jpg'), 'rb') as file2:
+                expected = exif.Image(file1).get_all()
+                actual = exif.Image(file2).get_all()
+                self.assertDictEqual(expected, expected)
